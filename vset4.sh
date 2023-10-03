efa9599a5c84f8e847a2c45a3c0e2d33124f735b
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2023-10-04 04:41:01 +0800

    Update vset4.sh

--- vset4.sh
+++ vset4.sh
@@ -4588,33 +4588,6 @@ ipset add blocked_ips 101.51.121.141
 ipset add blocked_ips 1.180.49.222 
 ipset add blocked_ips 1.179.130.201 
 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL ALL -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL NONE -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
- iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP  
- iptables -t mangle -D PREROUTING -s 224.0.0.0/3 -j DROP 
- iptables -t mangle -D PREROUTING -s 169.254.0.0/16 -j DROP 
- iptables -t mangle -D PREROUTING -s 172.16.0.0/12 -j DROP 
- iptables -t mangle -D PREROUTING -s 192.0.2.0/24 -j DROP 
- iptables -t mangle -D PREROUTING -s 192.168.0.0/16 -j DROP 
- iptables -t mangle -D PREROUTING -s 10.0.0.0/8 -j DROP 
- iptables -t mangle -D PREROUTING -s 0.0.0.0/8 -j DROP 
- iptables -t mangle -D PREROUTING -s 240.0.0.0/5 -j DROP 
- iptables -t mangle -D PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP 
-
- sleep 2
- iptables -I INPUT 1 -m set --match-set blocked_ips src -j DROP
- iptables-save
-
  
  # Make checker script executable
  chmod +x /root/checker.sh
@@ -4639,7 +4612,8 @@ ufw allow 65533/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw enable
-
+iptables -I INPUT 1 -m set --match-set blocked_ips src -j DROP
+iptables-save
 }
 
 function ScriptMessage(){
